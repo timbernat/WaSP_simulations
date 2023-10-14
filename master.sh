ec21b03e1f32daa923e21e9492bc5a6e71b69d16
Timotej Bernat <tiibe3324@colorado.edu>
2023-10-13 18:18:03 -0600

    Updated sbatch scripts to parallelize workflow submission

--- master.sh
+++ master.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-echo $PATH > fake_path.txt
+#!/bin/sh
 
 # ============================================================================================================================
 # DESC : orchestrates parallel SBATCH dispatch of entire chemistry- charge- and replicate-matrix simulation battery
@@ -11,7 +9,7 @@ echo $PATH > fake_path.txt
 # parameter definitions
 # ============================================================================================================================
 slurm_log_dir='slurm_logs'
-root_dir='sbatch_test'
+root_dir='wasp_sims'
 clear_existing=true
 
 struct_src='polymer_structures'
@@ -30,7 +28,7 @@ rct_charge_method='Espaloma-AM1-BCC'
 # rct_charge_method='AM1-BCC-ELF10'
 charge_methods=(
     # 'AM1-BCC-ELF10'
-    # 'Espaloma-AM1-BCC'
+    'Espaloma-AM1-BCC'
     'RCT'
 )
 
@@ -70,7 +68,6 @@ fi
 
 mkdir -p $root_dir # make root directory (exists ok)
 echo "Touched directory $root_dir"
-
 mkdir -p $slurm_log_dir
 
 # ============================================================================================================================
@@ -79,26 +76,26 @@ mkdir -p $slurm_log_dir
 for mol_name in "${mol_names[@]}"; do # iterate over list of names
     mol_dir=$root_dir/$mol_name
     root_log_dir="$mol_dir/Logs"
+    ioparam=$mol_dir/argio.txt # temporary text file for passing data between sbatch calls
 
-    param_args="$mol_name $mol_dir $root_log_dir $pdb_src/$mol_name.pdb $mono_src/$mol_name.json $N_max $rct_charge_method"
-    echo $param_args
-    job_id_param=$(sbatch --parsable --job-name par_$mol_name --output $slurm_log_dir/param_$mol_name.log param.job $param_args )
-    echo $job_id_param
+    param_args="$mol_dir $root_log_dir $ioparam $mol_name $pdb_src/$mol_name.pdb $mono_src/$mol_name.json $N_max $rct_charge_method"
+    job_id_param=$(sbatch --parsable --job-name par_$mol_name --output ${slurm_log_dir}/param_${mol_name}.log param.job ${param_args} )
 
-    # for charge_method in "${charge_methods[@]}"; do
-    #     charge_dir=$mol_dir/$charge_method
-    #     charge_log_dir="$charge_dir/Logs"
+    for charge_method in "${charge_methods[@]}"; do
+        charge_dir=$mol_dir/$charge_method
+        charge_log_dir="$charge_dir/Logs"
+        iocharge=$charge_dir/argio.txt
 
-    #     charge_path=$(python -m components.charge_mol -wdir $charge_dir -ldir $charge_log_dir  -sdf $sdf_path -cmet $charge_method -lc $lib_chg_path)
+        charge_args="$charge_dir $charge_log_dir $ioparam $iocharge $charge_method"
+        job_id_charge=$(sbatch --parsable --dependency "afterok:$job_id_param" --job-name $charge_method --output ${slurm_log_dir}/charge_${mol_name}_${charge_method}.log charge.job $charge_args)
 
-    #     for ((i=1; i<=$num_confs; i++)); do # can't use {1..$num_confs} (variable upper bound returns curly braces as literal)
-    #         conf_name="conformer_$i"
-    #         conf_dir=$charge_dir/$conf_name
-    #         conf_log_dir="$conf_dir/Logs"
+        for ((i=1; i<=$num_confs; i++)); do # can't use {1..$num_confs} (variable upper bound returns curly braces as literal)
+            conf_name="conf$i"
+            conf_dir=$charge_dir/$conf_name
+            conf_log_dir="$conf_dir/Logs"
 
-    #         conf_path=$(python -m components.anneal  -wdir $conf_dir -ldir $conf_log_dir -sdf $charge_path -a $conf_name -bd $box_x $box_y $box_z -bdu $box_unit -sp $anneal_param_path -ff $forcefield)
-    #         solv_path=$(python -m components.solvate -wdir $conf_dir -ldir $conf_log_dir -sdf $conf_path   -a $conf_name -solv $solvent -rho $density_gcm3 -bd $box_x $box_y $box_z -bdu $box_unit -exc $exclusion -excu $exclusion_unit)
-    #         python -m components.simulate -wdir $conf_dir -ldir $conf_log_dir -sdf $solv_path -a $conf_name -bd $box_x $box_y $box_z -bdu $box_unit -sp "equilibration=$equil_param_path" "production=$prod_param_path" -ff $forcefield -az 'production' -rmin $rmin -rmax $rmax -ru $rad_unit
-    #     done
-    # done
+            conf_args="$conf_dir $conf_log_dir $iocharge $conf_name $box_x $box_y $box_z $box_unit density_gcm3 $solvent $exclusion $exclusion_unit $forcefield $anneal_param_path $equil_param_path $prod_param_path $rmin $rmax $rad_unit"
+            sbatch --dependency "afterok:$job_id_charge" --job-name ${conf_name}${mol_name} --output ${slurm_log_dir}/${mol_name}_${charge_method}_${conf_name}.log  sims.job $conf_args # don't need to catch job ID (no subsequent jobs)
+        done
+    done
 done
